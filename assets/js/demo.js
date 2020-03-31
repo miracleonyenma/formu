7f1adbcb647b60be6c6856ee0074645ad197af61
miracleonyenma <miracleficient@gmail.com>
2020-04-01 00:40:05 +0100

    update: navigation to form examplse, navigation func

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,7 +1,15 @@
 
-var formContainers = document.querySelectorAll(".form-container");
+var formContainers = document.querySelectorAll(".form-container"),
+    demoNav = document.querySelector("#nav"),
+    formExamplesNav =  document.querySelector("#form-examples"),
+    demoNavBtn = document.querySelector("#navbtn");
 
-function getDemoFormTitles(elCont, el){
+function getDemoFormTitles(elCont, el, targetEl){
+    //remove all items before populating with new iiems
+    while(targetEl.firstElementChild){
+        targetEl.removeChild(targetEl.firstElementChild);
+    }
+
     for(var i = 0; i < elCont.length; i++){
         //get the text from the target element, set it to lowrcase
         //remove trailing whitespace using trim()
@@ -9,9 +17,33 @@ function getDemoFormTitles(elCont, el){
         //regEX expression matches all ("/g") whitespace "\s" and replaces it with "-"
         var titleText = elCont[i].querySelector(el).textContent.toLowerCase().trim().replace(/\s/g, "-");
         elCont[i].setAttribute("id", titleText);
+
+        var link = document.createElement("a");
+        var listItem = document.createElement("li");
+        link.textContent = titleText;
+        link.setAttribute("href", "#"+titleText);
+        listItem.appendChild(link);
+        targetEl.appendChild(listItem);
+    };
+    for (let j = 0; j < targetEl.querySelectorAll("li a").length; j++) {
+        const linkEl = targetEl.querySelectorAll("li a")[j];
+        linkEl.addEventListener("click", function(){
+            indicateForm(linkEl);
+        })
     }
 }
 
+function indicateForm(link){
+    var linkTarget = document.querySelector(link.getAttribute("href"));
+    linkTarget.classList.add("active");
+    setTimeout(function(){
+        linkTarget.classList.remove("active");
+    }, 800)
+}
+
+function navFunc(){
+    demoNav.classList.toggle("nav-active");
+}
 
 //run code when the dom is interactive - complete
 document.addEventListener('readystatechange', function(e){
@@ -20,6 +52,13 @@ document.addEventListener('readystatechange', function(e){
         console.log(e.target.readyState);
 	}
 	if(e.target.readyState === "complete"){
-        getDemoFormTitles(formContainers, ".section-title h2");
+        getDemoFormTitles(formContainers, ".section-title h2", formExamplesNav.querySelector("ul"));
 	}
 });
+
+demoNavBtn.addEventListener("click", navFunc);
+window.addEventListener("click", function(e){
+    if(e.target === demoNav){
+        navFunc();
+    }
+})
